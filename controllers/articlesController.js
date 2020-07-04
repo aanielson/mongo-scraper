be46e4618def26f381844de533687a666cc1d55b
Abigail Nielson <aa.nielson18@gmail.com>
2020-07-04 15:37:14 -0600

    mirrored structure from Saturdays scrape. added routes and models

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,49 +1,12 @@
 var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
+var router = express.Router();
 var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
-
-var PORT = 3000;
-
-// Initialize Express
-var app = express();
-
-// Set Handlebars....
-// var exphbs = require("express-handlebars");
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/articlesController.js");
-
-// Configure middleware
-
-// Use logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
-
-// Article Scraping Routes
+// Create the router for the app, and export the router at the end of your file.
 
 // A GET route for scraping the NEWS website
-app.get("/scrape", function (req, res) {
+router.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
-    axios.get("h#").then(function (response) {
+    axios.get("#").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
@@ -76,9 +39,8 @@ app.get("/scrape", function (req, res) {
         res.send("Scrape Complete");
     });
 });
-
 // Route for getting all Articles from the db
-app.get("/articles", function (req, res) {
+router.get("/articles", function (req, res) {
     // TODO: Finish the route so it grabs all of the articles
     db.Article.find({})
         .then(function (dbArticles) {
@@ -91,8 +53,8 @@ app.get("/articles", function (req, res) {
         });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's COMMENT
-app.get("/articles/:id", function (req, res) {
+// Route for grabbing a specific Article by id, populate it with it's comment
+router.get("/articles/:id", function (req, res) {
     // TODO
     // ====
     db.Article.findOne({ where: { id: req.params.id } })
@@ -105,16 +67,16 @@ app.get("/articles/:id", function (req, res) {
             res.json(err);
         });
     // Finish the route so it finds one article using the req.params.id,
-    // and run the populate method with "comment",
+    // and run the populate method with "commment",
     // then responds with the article with the comment included
 });
 
-// Route for saving/updating an Article's associated COMMENT
-app.post("/articles/:id", function (req, res) {
-    // save the new comment that gets posted to the COMMENT collection
+// Route for saving/updating an Article's associated comment
+router.post("/articles/:id", function (req, res) {
+    // save the new comment that gets posted to the Comments collection
     db.Comment.create(req.body)
         // then find an article from the req.params.id
-        .then(function (dbComment) {
+        .then(function (Comment) {
             return db.Article.findOneAndUpdate({}, { $push: { comment: dbComment._id } }, { new: true });
         })
         .then(function (dbArticle) {
@@ -125,10 +87,8 @@ app.post("/articles/:id", function (req, res) {
             // If an error occurs, send it back to the client
             res.json(err);
         });
-    // and update it's "COMMENT" property with the _id of the new COMMENT
+    // and update it's "comment" property with the _id of the new comment
 });
 
-// Start the server
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
-});
+// Export routes for server.js to use.
+module.exports = router;
